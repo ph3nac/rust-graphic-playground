8fef13a39b70b62a91e2aa6fe66e98d7f77c6fec
ph3 <100564304+ph3nac@users.noreply.github.com>
2024-08-16 09:18:24 +0900

    init window

---
+++ tiny-skia/src/main.rs
@@ -0,0 +1,25 @@
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::Window;
+fn main() {
+    let event_loop = EventLoop::new().unwrap();
+
+    let window = event_loop
+        .create_window(Window::default_attributes())
+        .unwrap();
+
+    event_loop.set_control_flow(ControlFlow::Wait);
+
+    let _ = event_loop.run(move |event, elwt| match event {
+        Event::WindowEvent {
+            event: WindowEvent::CloseRequested,
+            ..
+        } => elwt.exit(),
+        Event::AboutToWait => window.request_redraw(),
+        Event::WindowEvent {
+            window_id,
+            event: WindowEvent::RedrawRequested,
+        } if window_id == window.id() => {}
+        _ => (),
+    });
+}
