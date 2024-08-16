9fc14a840e6940d82b551e3575b2254dcb7e129b
ph3 <100564304+ph3nac@users.noreply.github.com>
2024-08-16 09:21:10 +0900

    add: some Todos

--- tiny-skia/src/main.rs
+++ tiny-skia/src/main.rs
@@ -4,12 +4,13 @@ use winit::window::Window;
 fn main() {
     let event_loop = EventLoop::new().unwrap();
 
+    // TODO use ActiveEventLoop
     let window = event_loop
         .create_window(Window::default_attributes())
         .unwrap();
 
+    // TODO use EventLoop::run_app
     event_loop.set_control_flow(ControlFlow::Wait);
-
     let _ = event_loop.run(move |event, elwt| match event {
         Event::WindowEvent {
             event: WindowEvent::CloseRequested,
